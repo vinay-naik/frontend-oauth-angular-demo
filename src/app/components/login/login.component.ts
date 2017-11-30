20ecf49096d89b4c97226dfaa4badaca2d46c80e
Vinay Naik <vinay@browntape.com>
2017-11-30 16:21:33 +0530

    Base functionality created for login.

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { GoogleAuth } from './auth/googleAuth.service';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material';
+
+
+@Component({
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css'],
+	host: { 'class': 'layout-fill display-block' },
+	providers: [GoogleAuth]
+})
+export class LoginComponent implements OnInit {
+
+	private response = {};
+	private returnUrl = '/dashboard';
+	constructor(
+		private route: ActivatedRoute,
+		public snackBar: MatSnackBar,
+		private googleAuth: GoogleAuth,
+		private router: Router) { 
+
+	}
+
+	openSnackBar(message: string, duration: number = 2000, action: string = '') {
+		let config = {
+			duration			: duration,
+			verticalPosition	: 'top' as MatSnackBarVerticalPosition,
+			horizontalPosition	: 'end' as MatSnackBarHorizontalPosition
+		};
+		this.snackBar.open(message, action, config);
+	}
+
+	authenticateCallback = response => {
+		console.log(response);
+		if(response.success) {
+			console.log("Here");
+			this.router.navigate([this.returnUrl]);
+		}
+	}
+
+	authenticate = (provider) => {
+		switch (provider) {
+			case 'google':
+				this.googleAuth.userSignIn().subscribe(this.authenticateCallback);
+				break;
+
+			default:
+				this.openSnackBar('The provider "' + provider + '" has not been integrated yet.');
+
+				break;
+		}
+	};
+
+	logOut = () => {
+		localStorage.setItem('forntend-oauth-user-key', '');
+		localStorage.setItem('forntend-oauth-user-name', '');
+		localStorage.setItem('forntend-oauth-user-image', '');
+		localStorage.setItem('forntend-oauth-user-email', '');
+	}
+
+	ngOnInit() {
+		this.logOut();
+		this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/dashboard';
+	}
+
+}
